3d6513fb19ae4cb5f35824dce4e5fc289505a787
jasmaa <jasonmaa3955@gmail.com>
2020-12-23 08:10:26 -0500

    Add refresh indicator for articles

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hareclip/models/article.dart';
 import 'package:hareclip/screens/article.dart';
 import 'package:hareclip/screens/home.dart';
-import 'package:hareclip/services/api.dart';
 
 void main() {
   runApp(
-    App(),
-  );
-}
-
-class App extends StatefulWidget {
-  App({Key key}) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  Future<List<Article>> futureArticles;
-
-  @override
-  void initState() {
-    super.initState();
-    futureArticles = fetchArticles();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+    MaterialApp(
       routes: {'/article': (context) => ArticleScreen()},
       title: 'Hareclip',
-      home: HomeScreen(futureArticles: this.futureArticles),
-    );
-  }
+      home: HomeScreen(),
+    ),
+  );
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:hareclip/components/articleCard.dart';
 import 'package:hareclip/models/article.dart';
+import 'package:hareclip/services/api.dart';
 
-class HomeScreen extends StatelessWidget {
-  final Future<List<Article>> futureArticles;
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-  HomeScreen({Key key, @required this.futureArticles}) : super(key: key);
+class _HomeScreenState extends State<HomeScreen> {
+  List<Article> _articles = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // TODO: put in a loading screen?
+    fetchArticles().then((articles) {
+      setState(() {
+        _articles = articles;
+      });
+    });
+  }
+
+  Future<void> _loadArticles() async {
+    try {
+      var articles = await fetchArticles();
+      setState(() {
+        _articles = articles;
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: FutureBuilder(
-        future: futureArticles,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                var article = snapshot.data[index];
-                return ArticleCard(article: article);
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        },
+      body: RefreshIndicator(
+        onRefresh: _loadArticles,
+        child: ListView.builder(
+          itemCount: _articles.length,
+          itemBuilder: (context, index) {
+            return ArticleCard(article: _articles[index]);
+          },
+        ),
       ),
     );
   }
