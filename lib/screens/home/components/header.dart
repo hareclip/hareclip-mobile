a03eee7af64df3750955d2f4a7a6a0f356269d72
jasmaa <jasonmaa3955@gmail.com>
2020-12-24 14:23:55 -0500

    Add category filtering. Redo infinite scroll.

--- lib/screens/home/components/header.dart
+++ lib/screens/home/components/header.dart
@@ -2,61 +2,81 @@ import 'package:flutter/material.dart';
 
 // HomeHeader is home screen header with search bar and profile
 class HomeHeader extends StatelessWidget {
+  final List<String> categories;
+  final int selectedCategory;
+  final Function setSelectedCategory;
+
+  HomeHeader(
+      {Key key,
+      @required this.selectedCategory,
+      @required this.categories,
+      @required this.setSelectedCategory})
+      : super(key: key);
+
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(
-        left: 10,
-        top: 10,
-        right: 10,
-        bottom: 10,
-      ),
-      color: Theme.of(context).primaryColor,
-      child: Padding(
-        padding: const EdgeInsets.only(
-          left: 10,
-          top: 10,
-          right: 10,
-          bottom: 10,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Icon(
-              Icons.person,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      color: Theme.of(context).backgroundColor,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  Icons.person,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                      decoration: new InputDecoration(
+                          contentPadding: const EdgeInsets.only(
+                            left: 10,
+                            top: 0,
+                            right: 10,
+                            bottom: 0,
+                          ),
+                          border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                              const Radius.circular(10.0),
+                            ),
+                          ),
+                          filled: true,
+                          hintStyle: new TextStyle(color: Colors.grey[800]),
+                          hintText: "Search",
+                          fillColor: Colors.white),
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.person,
+                ),
+              ],
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  top: 0,
-                  right: 10,
-                  bottom: 0,
-                ),
-                child: TextField(
-                  decoration: new InputDecoration(
-                      contentPadding: const EdgeInsets.only(
-                        left: 10,
-                        top: 0,
-                        right: 10,
-                        bottom: 0,
-                      ),
-                      border: new OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(10.0),
-                        ),
-                      ),
-                      filled: true,
-                      hintStyle: new TextStyle(color: Colors.grey[800]),
-                      hintText: "Search",
-                      fillColor: Colors.white),
-                ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: List.generate(
+                categories.length,
+                (index) => FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    color: selectedCategory == index
+                        ? Theme.of(context).primaryColor
+                        : Theme.of(context).backgroundColor,
+                    child: Text(categories[index]),
+                    onPressed: () {
+                      setSelectedCategory(index);
+                    }),
               ),
             ),
-            Icon(
-              Icons.person,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -12,37 +12,41 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final int _numFetch = 10;
+  final List<String> _categories = [
+    'All',
+    'News',
+    'Features',
+    'Sports',
+    'Entertainment',
+    'Rabbithole',
+  ];
+
   List<Article> _articles = [];
+  List<Article> _filteredArticles = [];
   ScrollController _scrollController = new ScrollController();
   int _currentOffset = 0;
+  int _selectedCategory = 0;
+  bool _isLoading = false;
+  bool _hasMore = true;
 
   @override
   void initState() {
     super.initState();
-
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        _getMoreArticles();
-      }
-    });
-
-    // TODO: put in a loading screen?
-    fetchArticles(_numFetch, 0).then((articles) {
-      setState(() {
-        _articles = articles;
-      });
-    });
-    _currentOffset += _numFetch;
   }
 
-  // Load initial articles on refresh
-  Future<void> _loadInitialArticles() async {
+  // Reload articles on refresh
+  Future<void> _reloadArticles() async {
     try {
       var articles = await fetchArticles(_numFetch, 0);
       _currentOffset = _numFetch;
       setState(() {
         _articles = articles;
+        if (_selectedCategory != 0) {
+          _filteredArticles = articles
+              .where((article) =>
+                  article.categoryLabel == _categories[_selectedCategory])
+              .toList();
+        }
       });
     } catch (err) {
       throw err;
@@ -52,45 +56,84 @@ class _HomeScreenState extends State<HomeScreen> {
   // Get more articles for infinite scroll
   _getMoreArticles() async {
     try {
+      _isLoading = true;
       var articles = await fetchArticles(_numFetch, _currentOffset);
       _currentOffset += _numFetch;
       setState(() {
         _articles.addAll(articles);
+        if (_selectedCategory != 0) {
+          _filteredArticles.addAll(articles
+              .where((article) =>
+                  article.categoryLabel == _categories[_selectedCategory])
+              .toList());
+        }
+
+        if (articles.isEmpty) {
+          _hasMore = false;
+        }
+
+        _isLoading = false;
       });
     } catch (err) {
       throw err;
     }
   }
 
+  // Set selected category and filter articles
+  _setSelectedCategory(int category) {
+    setState(() {
+      _scrollController.animateTo(1,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
+      _selectedCategory = category;
+      if (category != 0) {
+        _filteredArticles = _articles
+            .where((article) =>
+                article.categoryLabel == _categories[_selectedCategory])
+            .toList();
+      }
+    });
+  }
+
   Widget build(BuildContext context) {
+    List<Article> displayArticles =
+        _selectedCategory == 0 ? _articles : _filteredArticles;
+
     return SafeArea(
       child: Scaffold(
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            HomeHeader(),
+            HomeHeader(
+              categories: _categories,
+              selectedCategory: _selectedCategory,
+              setSelectedCategory: _setSelectedCategory,
+            ),
             Expanded(
               child: RefreshIndicator(
-                onRefresh: _loadInitialArticles,
+                onRefresh: _reloadArticles,
                 child: ListView.builder(
                   controller: _scrollController,
-                  itemCount: _articles.length + 1,
+                  itemCount: _hasMore
+                      ? displayArticles.length + 1
+                      : displayArticles.length,
                   itemBuilder: (context, index) {
-                    if (index < _articles.length) {
-                      return ArticleCard(article: _articles[index]);
-                    } else {
-                      // TODO: condition when no more left to load?
+                    if (index >= displayArticles.length) {
+                      // Render loading if not an article
+
+                      // Fetch more articles if not doing so already
+                      if (!_isLoading) {
+                        _getMoreArticles();
+                      }
+
                       return Center(
                         child: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 10,
-                            top: 10,
-                            right: 10,
-                            bottom: 10,
-                          ),
+                          padding: const EdgeInsets.all(10),
                           child: CircularProgressIndicator(),
                         ),
                       );
+                    } else {
+                      // Otherwise, render article card
+                      return ArticleCard(article: displayArticles[index]);
                     }
                   },
                 ),
